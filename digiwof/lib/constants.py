6eb675c57e1e49fcfd49922e422cc87abb4e2d3d
DigiDuncan <digiduncan@gmail.com>
2020-02-17 02:10:36 -0500

    Change the JSONs format.

--- digiwof/lib/constants.py
+++ digiwof/lib/constants.py
@@ -3,10 +3,20 @@ font = {
     "b": ":regional_indicator_b:",
     "c": ":regional_indicator_c:",
     "d": ":regional_indicator_d:"
-#...
+# ...
 # TODO: Finish this.
 }
 
 vowels = set("aeiou")
 consonants = set("bcdfghjklmnpqrstvwxyz")
 rounds = [-1, -2, 1, 2, 3 -3, 4, 0, 100]
+# Round list:
+# $1000 Toss-Up
+# $2000 Toos-Up
+# Round 1
+# Round 2
+# Round 3
+# $3000 Toss-Up
+# Round 4
+# Bonus Round
+# End
